11f0683090aab5fd1f0658a2a7db732a96a851a5
Emru <56933144+emrullahgit1@users.noreply.github.com>
2025-10-29 01:26:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/keymanager/main.go
@@ -0,0 +1,23 @@
+// cmd/keymanager/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "keymanager/internal/keymanager"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := keymanager.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
